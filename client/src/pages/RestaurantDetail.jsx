367e6e14bf6e72c125926b5a6ca10941ada9c4ec
rushi <rushikesh.k21@iiits.in>
2024-08-11 23:00:32 +0530

    backend server live

--- client/src/pages/RestaurantDetail.jsx
+++ client/src/pages/RestaurantDetail.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import Map from '../components/Map';
